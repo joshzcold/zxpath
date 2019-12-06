f1295d8614323d94db7737664587aded11082489
Joshua J Cold <joshua.cold@microfocus.com>
2019-12-06 14:29:03 -0700

    Made program use zxpath starting point

--- popup/tools.js
+++ popup/tools.js
@@ -1,98 +1,45 @@
-/**
- * CSS to hide everything on the page,
- * except for elements that have the "beastify-image" class.
- */
-const hidePage = `body > :not(.beastify-image) {
-                    display: none;
-                  }`;
-
-/**
- * Listen for clicks on the buttons, and send the appropriate message to
- * the content script in the page.
- */
 function listenForClicks() {
   document.addEventListener("click", (e) => {
 
-    /**
-     * Given the name of a beast, get the URL to the corresponding image.
-     */
-    function beastNameToURL(beastName) {
-      switch (beastName) {
-        case "Frog":
-          return browser.extension.getURL("beasts/frog.jpg");
-        case "Snake":
-          return browser.extension.getURL("beasts/snake.jpg");
-        case "Turtle":
-          return browser.extension.getURL("beasts/turtle.jpg");
-      }
+    function selectorMode(tabs) {
+      console.log("entering selector mode")
     }
 
-    /**
-     * Insert the page-hiding CSS into the active tab,
-     * then get the beast URL and
-     * send a "beastify" message to the content script in the active tab.
-     */
-    function beastify(tabs) {
-      browser.tabs.insertCSS({code: hidePage}).then(() => {
-        let url = beastNameToURL(e.target.textContent);
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "beastify",
-          beastURL: url
-        });
-      });
+    function openSettings(){
+      console.log("opening settings page")
     }
 
-    /**
-     * Remove the page-hiding CSS from the active tab,
-     * send a "reset" message to the content script in the active tab.
-     */
-    function reset(tabs) {
-      browser.tabs.removeCSS({code: hidePage}).then(() => {
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "reset",
-        });
-      });
+    function openSave(){
+      console.log("opening save popup")
     }
 
-    /**
-     * Just log the error to the console.
-     */
-    function reportError(error) {
-      console.error(`Could not beastify: ${error}`);
-    }
-
-    /**
-     * Get the active tab,
-     * then call "beastify()" or "reset()" as appropriate.
-     */
-    if (e.target.classList.contains("beast")) {
+    if (e.target.classList.contains("select")) {
       browser.tabs.query({active: true, currentWindow: true})
-        .then(beastify)
+        .then(selectorMode)
         .catch(reportError);
     }
-    else if (e.target.classList.contains("reset")) {
+    else if (e.target.classList.contains("list")) {
       browser.tabs.query({active: true, currentWindow: true})
-        .then(reset)
+        .then(openSave)
         .catch(reportError);
+    }
+    else if (e.target.classList.contains("settings")) {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(openSettings)
+        .catch(reportError);
+    } 
+    else {
+      console.error("not a valid button selection")
     }
   });
 }
 
-/**
- * There was an error executing the script.
- * Display the popup's error message, and hide the normal UI.
- */
 function reportExecuteScriptError(error) {
   document.querySelector("#popup-content").classList.add("hidden");
   document.querySelector("#error-content").classList.remove("hidden");
   console.error(`Failed to execute beastify content script: ${error.message}`);
 }
 
-/**
- * When the popup loads, inject a content script into the active tab,
- * and add a click handler.
- * If we couldn't inject the script, handle the error.
- */
 browser.tabs.executeScript({file: "/content_scripts/main.js"})
 .then(listenForClicks)
 .catch(reportExecuteScriptError);
