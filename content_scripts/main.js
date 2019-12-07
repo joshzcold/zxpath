83ea2145bffa76e237434dae54b8804054474bac
Joshua J Cold <joshua.cold@microfocus.com>
2019-12-06 17:00:44 -0700

    made the freakin buttons work

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,37 +1,17 @@
 (function() {
-  /**
-   * Check and set a global guard variable.
-   * If this content script is injected into the same page again,
-   * it will do nothing next time.
-   */
   if (window.hasRun) {
     return;
   }
   window.hasRun = true;
 
-  /**
-   * Given a URL to a beast image, remove all existing beasts, then
-   * create and style an IMG node pointing to
-   * that image, then insert the node into the document.
-   */
   function insertBeast(beastURL) {
-    removeExistingBeasts();
-    let beastImage = document.createElement("img");
-    beastImage.setAttribute("src", beastURL);
-    beastImage.style.height = "100vh";
-    beastImage.className = "beastify-image";
-    document.body.appendChild(beastImage);
+    /*
+    EXECUTE STUFF HERE YAH DUMMY!!!
+    */
+    console.log("DUUUUUUDDDDDEEE!")
   }
 
-  /**
-   * Remove every beast from the page.
-   */
-  function removeExistingBeasts() {
-    let existingBeasts = document.querySelectorAll(".beastify-image");
-    for (let beast of existingBeasts) {
-      beast.remove();
-    }
-  }
+
 
   /**
    * Listen for messages from the background script.
@@ -40,9 +20,7 @@
   browser.runtime.onMessage.addListener((message) => {
     if (message.command === "beastify") {
       insertBeast(message.beastURL);
-    } else if (message.command === "reset") {
-      removeExistingBeasts();
-    }
+    } 
   });
 
 })();
--- popup/tools.js
+++ popup/tools.js
@@ -1,27 +1,46 @@
+/**
+* Listen for clicks on the buttons, and send the appropriate message to
+* the content script in the page.
+*/
+function listenForClicks() {
+document.addEventListener("click", (e) => {
 
-function popupActions(){
 
-  console.log("Hello World")
-
-  document.getElementById("zxpath-select-button").addEventListener("click", function(){
-    console.log("entering selector mode")
-  }); 
-  
-  document.getElementById("zxpath-download-button").addEventListener("click", function(){
-    console.log("opening save popup")
-  }); 
-  
-  document.getElementById("zxpath-settings-button").addEventListener("click", function(){
-    console.log("opening settings page")
-  }); 
+function sendCommand(tabs) {
+  browser.tabs.sendMessage(tabs[0].id, {
+    command: "beastify",
+    });
 }
 
+
+
+function reportError(error) {
+console.error(`Could not beastify: ${error}`);
+}
+
+if (e.target.classList.contains("select")) {
+browser.tabs.query({active: true, currentWindow: true})
+.then(sendCommand)
+.catch(reportError);
+}
+});
+}
+
+/**
+* There was an error executing the script.
+* Display the popup's error message, and hide the normal UI.
+*/
 function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute: ${error.message}`);
+document.querySelector("#popup-content").classList.add("hidden");
+document.querySelector("#error-content").classList.remove("hidden");
+console.error(`Failed to execute beastify content script: ${error.message}`);
 }
 
+/**
+* When the popup loads, inject a content script into the active tab,
+* and add a click handler.
+* If we couldn't inject the script, handle the error.
+*/
 browser.tabs.executeScript({file: "/content_scripts/main.js"})
-.then(popupActions)
+.then(listenForClicks)
 .catch(reportExecuteScriptError);
