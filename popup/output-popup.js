5af173eb7ae36e2524cd0f78a9ef722a13e7ab7b
ZHall <zachary.hall@microfocus.com>
2019-12-10 15:03:44 -0700

    modal update

--- popup/output-popup.js
+++ popup/output-popup.js
@@ -1,17 +1,19 @@
 //To copy to the clipboard, we need to have an element with the text to be copied
-function copy() {
+// function copy() {
     
-    var copyText = document.querySelector("#xpath-text");
-    copyText.select();
-    document.execCommand("copy");
-  }
+//     var copyText = document.querySelector("#xpath-text");
+//     copyText.select();
+//     document.execCommand("copy");
+//   }
 
-  function save() {
-      let fileName = "xpaths";
-      let text = "//div[@id='I work'";
-      let blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-      saveAs(blob, fileName);
+//   function save() {
+//       let fileName = "xpaths";
+//       let text = "//div[@id='I work'";
+//       let blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+//       saveAs(blob, fileName);
     
+
+
     //   let element = document.createElement('a');
     //   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     //   element.setAttribute('download', fileName);
@@ -22,8 +24,8 @@ function copy() {
     //   element.click();
     
     //   document.body.removeChild(element);
-  }
+  // }
   
-  document.querySelector("#copy-btn").addEventListener("click", copy);
+  // document.querySelector("#copy-btn").addEventListener("click", copy);
 
-  document.querySelector("#save-btn").addEventListener("click", save);
+  // document.querySelector("#save-btn").addEventListener("click", save);
