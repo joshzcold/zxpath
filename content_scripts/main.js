f9ee44ad06f535a209f266fd63a96b37584260c1
Reed McFadden <reed.mcfadden@microfocus.com>
2019-12-10 16:19:07 -0700

    re-styled dropdown. working on custom drop down event handlers. still need to get popup to appear on clicks

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -81,6 +81,14 @@
    try {
     var div = document.createElement("div");
     div.innerHTML='<object type="text/html" data="../element-popout/element.html"></object>';
+    div.style.position = "absolute";
+    div.style.left = X.left;
+    console.log(X + X.left + X.top);
+    console.log(div);
+    div.style.top = Y.top;
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.zIndex = "60000000"
     document.getElementById("xpath-overlay").appendChild(div);
 
    } catch (error) {
