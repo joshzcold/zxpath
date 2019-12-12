8527d89521af30cfe55a4ced7a9162d5d399745c
Reed McFadden <reed.mcfadden@microfocus.com>
2019-12-12 10:54:41 -0700

    setting up event handlers for xpath selections

--- element-popout/element.js
+++ element-popout/element.js
@@ -1,19 +1,31 @@
 (function () {
+
+
     function handleResponse(message) {
         console.log("message received in element.js");
     }
-    
+
     function handleError(error) {
         console.log("error message sending in element.js");
     }
-    
-    document.addEventListener("click", (e) => {
+
+    document.getElementById('dropdown-tooltip').addEventListener("click", (e) => {
         console.log("HELLO, inside of click event listener of element.js")
         let response = browser.runtime.sendMessage({
+            command: "elementCommand",
             content: "message originating from element.js",
-            command: "elementCommand"
         });
-    
+
         response.then(handleResponse, handleError);
     });
-    })();
+
+    document.getElementById('name').addEventListener("click", (e) => {
+        console.log("HELLO, inside of click event listener of element.js")
+        let response = browser.runtime.sendMessage({
+            command: "elementCommand",
+            content: "message originating from element.js",
+        });
+
+        response.then(handleResponse, handleError);
+    });
+})();
